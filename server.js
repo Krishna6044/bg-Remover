ab75f54561bb70aa39ed358c862c487f94c51abb
Krishna6044 <guptak6593@gmail.com>
2026-09-01 09:51:40 +0530

    Add new origin to CORS configurationfix cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 4000
 
-app.use(cors({ origin: ['http://localhost:5173', 'http://127.0.0.1:5173'] }))
+app.use(cors({ origin: ['http://localhost:5173', 'http://127.0.0.1:5173','https://bgremover-h69h.onrender.com'] }))
 app.use(express.json({ limit: '10mb' }))
 
 const requiredEnv = [
